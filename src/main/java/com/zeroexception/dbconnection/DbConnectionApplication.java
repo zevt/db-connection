bc4b4715e8c7d34d2b69d6687722f8e8a56715bf
Viet Quoc Tran <vietsemantics@gmail.com>
2018-03-22 09:45:36 -0500

    init

---
+++ src/main/java/com/zeroexception/dbconnection/DbConnectionApplication.java
@@ -0,0 +1,12 @@
+package com.zeroexception.dbconnection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbConnectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbConnectionApplication.class, args);
+	}
+}
---
+++ src/test/java/com/zeroexception/dbconnection/DbConnectionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zeroexception.dbconnection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DbConnectionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
