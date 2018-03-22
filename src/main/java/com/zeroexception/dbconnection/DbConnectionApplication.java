1d8e7c96b27ed0562d06c1cc2f44d07fa377a2e3
Viet Quoc Tran <vietsemantics@gmail.com>
2018-03-22 11:30:34 -0500

    connect to MongoDB and Test

--- src/main/java/com/zeroexception/dbconnection/DbConnectionApplication.java
+++ src/main/java/com/zeroexception/dbconnection/DbConnectionApplication.java
@@ -1,12 +1,27 @@
 package com.zeroexception.dbconnection;
 
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 @SpringBootApplication
-public class DbConnectionApplication {
+public class DbConnectionApplication implements CommandLineRunner {
+
+	@Autowired
+	private MongoTemplate mongoTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DbConnectionApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		ObjectNode node = JsonNodeFactory.instance.objectNode();
+		node.put("message","test");
+		this.mongoTemplate.save(node, "test");
 	}
 }
---
+++ src/test/java/com/zeroexception/dbconnection/MongoDBConnectionTest.java
@@ -0,0 +1,29 @@
+package com.zeroexception.dbconnection;
+
+
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@TestPropertySource(locations = {"classpath:application-local.properties"})
+public class MongoDBConnectionTest {
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @Test
+    public void testMongoConnection() {
+        ObjectNode node = JsonNodeFactory.instance.objectNode();
+        node.put("message","test");
+        this.mongoTemplate.save(node, "test");
+    }
+
+}
